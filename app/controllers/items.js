a007209951b70e9e14c4ed7fbb099ffbd1141951
Phil Gengler <philip.gengler@nytimes.com>
2014-06-13 11:32:36 -0400

    Allow new items to be created

--- app/controllers/items.js
+++ app/controllers/items.js
@@ -1,3 +1,36 @@
 export default Ember.ArrayController.extend({
-    sortProperties: [ 'date', 'startTime', 'endTime', 'event', 'location' ]
+    sortProperties: [ 'date', 'startTime', 'endTime', 'event', 'location' ],
+    actions: {
+        createItem: function() {
+            var event = this.get('newEvent').trim();
+            if (!event) {
+                return;
+            }
+            var location = this.get('newLocation');
+            if (location) {
+                location = location.trim();
+            }
+            var startTime = this.get('newStartTime');
+            if (startTime) {
+                startTime = startTime.trim();
+            }
+            var endTime = this.get('newEndTime');
+            if (endTime) {
+                endTime = endTime.trim();
+            }
+
+            var item = this.store.createRecord('item', {
+                'event': event,
+                'location': location,
+                'startTime': startTime,
+                'endTime': endTime
+            });
+            item.save();
+
+            this.set('newEvent', '');
+            this.set('newLocation', '');
+            this.set('newStartTime', '');
+            this.set('newEndTime', '');
+        }
+    }
 });
