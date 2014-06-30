496e9ff1585155c306c45b33ffccdcc79123d9f5
Phil Gengler <phil@pgengler.net>
2014-06-30 13:21:49 -0400

    Change to display items grouped by date instead of all in a single list

--- app/controllers/items.js
+++ app/controllers/items.js
@@ -1,36 +1,55 @@
 export default Ember.ArrayController.extend({
-    sortProperties: [ 'date', 'startTime', 'endTime', 'event', 'location' ],
-    actions: {
-        createItem: function() {
-            var event = this.get('newEvent').trim();
-            if (!event) {
-                return;
-            }
-            var location = this.get('newLocation');
-            if (location) {
-                location = location.trim();
-            }
-            var startTime = this.get('newStartTime');
-            if (startTime) {
-                startTime = startTime.trim();
-            }
-            var endTime = this.get('newEndTime');
-            if (endTime) {
-                endTime = endTime.trim();
-            }
+  sortProperties: [ 'date', 'startTime', 'endTime', 'event', 'location' ],
+  actions: {
+    createItem: function() {
+      var event = this.get('newEvent').trim();
+      if (!event) {
+        return;
+      }
+      var location = this.get('newLocation');
+      if (location) {
+        location = location.trim();
+      }
+      var startTime = this.get('newStartTime');
+      if (startTime) {
+        startTime = startTime.trim();
+      }
+      var endTime = this.get('newEndTime');
+      if (endTime) {
+        endTime = endTime.trim();
+      }
 
-            var item = this.store.createRecord('item', {
-                'event': event,
-                'location': location,
-                'startTime': startTime,
-                'endTime': endTime
-            });
-            item.save();
+      var item = this.store.createRecord('item', {
+        'event': event,
+        'location': location,
+        'startTime': startTime,
+        'endTime': endTime
+      });
+      item.save();
 
-            this.set('newEvent', '');
-            this.set('newLocation', '');
-            this.set('newStartTime', '');
-            this.set('newEndTime', '');
-        }
+      this.set('newEvent', '');
+      this.set('newLocation', '');
+      this.set('newStartTime', '');
+      this.set('newEndTime', '');
     }
+  },
+
+  groupedByDate: function () {
+    var result = [];
+
+    this.get('content').forEach(function(item) {
+      var hasDate = result.findBy('date', item.get('date'));
+
+      if (!hasDate) {
+        result.pushObject(Ember.Object.create({
+          date: item.get('date'),
+          contents: []
+        }));
+      }
+
+      result.findBy('date', item.get('date')).get('contents').pushObject(item);
+    });
+
+    return result;
+  }.property('content.[]')
 });
