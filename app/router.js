ce835f8d43768acc350128066ff0a3599060c61f
Phil Gengler <phil@pgengler.net>
2014-07-18 15:25:27 -0400

    Add support for tags, and restructure things a little so that there's an index route which shows the old "items" view (by date) as well as a new view of items by tag

--- app/router.js
+++ app/router.js
@@ -6,6 +6,7 @@ var Router = Ember.Router.extend({
 
 Router.map(function() {
   this.resource('items');
+  this.resource('tags');
 });
 
 export default Router;
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  redirect: function() {
-    this.transitionTo('items');
+  model: function() {
+    return Ember.RSVP.hash({
+      items: this.store.findAll('item'),
+      tags: this.store.findAll('tag')
+    });
   }
 });
