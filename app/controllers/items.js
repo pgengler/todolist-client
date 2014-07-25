d2648c2c2987b12120fe258bd2d7f8d8f2aca7b0
Phil Gengler <phil@pgengler.net>
2014-07-25 15:01:53 -0400

    Remove now-unused 'startTime', 'endTime', and 'location' properties for items

--- app/controllers/items.js
+++ app/controllers/items.js
@@ -2,38 +2,20 @@ import Ember from 'ember';
 import GroupableMixin from 'ember-todo/mixins/groupable';
 
 export default Ember.ArrayController.extend(GroupableMixin, {
-  sortProperties: [ 'date', 'startTime', 'endTime', 'event', 'location' ],
+  sortProperties: [ 'date', 'event' ],
   actions: {
     createItem: function() {
       var event = this.get('newEvent').trim();
       if (!event) {
         return;
       }
-      var location = this.get('newLocation');
-      if (location) {
-        location = location.trim();
-      }
-      var startTime = this.get('newStartTime');
-      if (startTime) {
-        startTime = startTime.trim();
-      }
-      var endTime = this.get('newEndTime');
-      if (endTime) {
-        endTime = endTime.trim();
-      }
 
       var item = this.store.createRecord('item', {
-        'event': event,
-        'location': location,
-        'startTime': startTime,
-        'endTime': endTime
+        'event': event
       });
       item.save();
 
       this.set('newEvent', '');
-      this.set('newLocation', '');
-      this.set('newStartTime', '');
-      this.set('newEndTime', '');
     }
   },
   groupBy: 'date'
--- app/models/item.js
+++ app/models/item.js
@@ -4,9 +4,6 @@ export default DS.Model.extend({
     done: DS.attr('boolean'),
     date: DS.attr('date'),
     event: DS.attr('string'),
-    location: DS.attr('string'),
-    startTime: DS.attr('number'),
-    endTime: DS.attr('number'),
 
     tags: DS.hasMany('tag')
 });
