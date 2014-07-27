fe6cdf7b0ae3b95929fb9e112c31bae936342c07
Phil Gengler <phil@pgengler.net>
2014-07-26 23:43:30 -0400

    Hacky way to get the items to display again

--- app/models/item.js
+++ app/models/item.js
@@ -5,5 +5,5 @@ export default DS.Model.extend({
     date: DS.attr('date'),
     event: DS.attr('string'),
 
-    item_tags: DS.hasMany('itemTag', { async: true })
+    itemTags: DS.hasMany('itemTag', { async: true })
 });
--- app/models/tag.js
+++ app/models/tag.js
@@ -1,6 +1,12 @@
+import Ember from 'ember';
 import DS from 'ember-data';
 
 export default DS.Model.extend({
   name: DS.attr('string'),
-  items: DS.hasMany('itemTag', { async: true })
+  itemTags: DS.hasMany('itemTag', { async: true }),
+  items: function(){
+    return this.get('itemTags').map(function(elem){
+      return elem.get('item');
+    });
+  }.property('itemTags.@each')
 });
