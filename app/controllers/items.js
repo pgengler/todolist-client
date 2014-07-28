7ab85830db510ef6f775fb4fa419d811c4e42208
Phil Gengler <phil@pgengler.net>
2014-07-28 12:55:35 -0400

    Remove unused 'createItem' action on items controller

--- app/controllers/items.js
+++ app/controllers/items.js
@@ -1,21 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
-  sortProperties: 'event',
-
-  actions: {
-    createItem: function() {
-      var event = this.get('newEvent').trim();
-      if (!event) {
-        return;
-      }
-
-      var item = this.store.createRecord('item', {
-        'event': event
-      });
-      item.save();
-
-      this.set('newEvent', '');
-    }
-  }
+  sortProperties: 'event'
 });
