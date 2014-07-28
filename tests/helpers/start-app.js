517ca869c06accf3eed28429a80171f0ac4f35ec
Phil Gengler <phil@pgengler.net>
2014-07-28 13:45:57 -0400

    Update packages

--- tests/helpers/start-app.js
+++ tests/helpers/start-app.js
@@ -2,6 +2,7 @@
 
 var Application = require('ember-todo/app')['default'];
 var Router = require('ember-todo/router')['default'];
+import Ember from 'ember';
 
 export default function startApp(attrs) {
   var App;
