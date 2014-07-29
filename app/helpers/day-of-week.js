ed449a3a5f5b43b129c9676865637e3cb13cb4e3
Phil Gengler <phil@pgengler.net>
2014-07-29 18:41:18 -0400

    Remove unused helpers

--- app/helpers/day-of-week.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Handlebars.makeBoundHelper(function(date) {
-	if (date) {
-		return moment(date).format('dddd');
-	}
-	return "";
-});
--- app/helpers/format-date.js
+++
@@ -1,8 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Handlebars.makeBoundHelper(function(value) {
-    if (value) {
-        return moment(value).format('MMMM DD, YYYY');
-    }
-    return "--";
-});
