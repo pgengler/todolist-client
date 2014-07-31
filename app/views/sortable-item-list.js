86ac355b231f083ef056dd64ee54ad646209617e
Phil Gengler <phil@pgengler.net>
2014-07-31 17:08:29 -0400

    Enable jQuery UI 'sortable' for lists of items

--- app/views/sortable-item-list.js
+++ app/views/sortable-item-list.js
@@ -3,5 +3,11 @@ import Ember from 'ember';
 export default Ember.View.extend({
 	classNames: [ 'sortable' ],
 	tagName: 'ul',
-	templateName: 'sortable-item-list'
+	templateName: 'sortable-item-list',
+
+	didInsertElement: function() {
+		var element = this.get('element');
+		this.$(element).sortable();
+		this.$(element).disableSelection();
+	}
 });
