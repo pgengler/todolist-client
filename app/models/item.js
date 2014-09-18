3875f6393749bc364ff2b176e6c5df2f3deaf87c
Phil Gengler <phil@pgengler.net>
2014-09-18 12:33:13 -0400

    Mark an item as done

--- app/models/item.js
+++ app/models/item.js
@@ -1,15 +1,28 @@
 import DS from 'ember-data';
 
 var Item = DS.Model.extend({
-    done: DS.attr('boolean'),
-    event: DS.attr('string')
+	done: DS.attr('boolean'),
+	event: DS.attr('string'),
+
+	isDone: function(key, value) {
+		if (typeof(value) === 'undefined') {
+			// property being used as a getter
+			return this.get('done');
+		} else {
+			// property being used as a setter
+			this.set('done', value);
+			this.save();
+			return value;
+		}
+	}.property('done')
 });
 
 Item.reopenClass({
-  FIXTURES: [
-    { id: 1, done: false, event: 'A thing' },
-    { id: 2, done: false, event: 'Something else' }
-  ]
+	FIXTURES: [
+		{ id: 1, done: false, event: 'A thing' },
+		{ id: 2, done: false, event: 'Something else' },
+		{ id: 3, done: true, event: 'A finished thing' }
+	]
 });
 
 export default Item;
