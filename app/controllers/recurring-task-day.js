781add7dbcbcc444e931b2d596a34ed32d3cfbb4
Phil Gengler <phil@pgengler.net>
2014-10-03 14:57:15 -0400

    Allow new recurring tasks to be added

--- app/controllers/recurring-task-day.js
+++ app/controllers/recurring-task-day.js
@@ -2,5 +2,23 @@ import Ember from 'ember';
 
 export default Ember.ObjectController.extend({
 	taskSortProperties: [ 'description' ],
-	sortedTasks: Ember.computed.sort('tasks', 'taskSortProperties')
+	sortedTasks: Ember.computed.sort('tasks', 'taskSortProperties'),
+
+	newTaskDescription: '',
+
+	actions: {
+		addTask: function() {
+			var description = this.get('newTaskDescription').trim();
+			if (!Ember.isEmpty(description)) {
+				var self = this;
+				this.store.createRecord('recurringTask', {
+					description: description,
+					day: this.get('model')
+				}).save().then(function(task) {
+					self.get('tasks').addObject(task);
+					self.set('newTaskDescription', '');
+				});
+			}
+		}
+	}
 });
