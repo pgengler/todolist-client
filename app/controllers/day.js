7cf90b11de27ceb783147f981d6b6505fd44bc0b
Phil Gengler <phil@pgengler.net>
2014-10-08 11:46:56 -0400

    Style tweaks

--- app/controllers/day.js
+++ app/controllers/day.js
@@ -3,6 +3,22 @@ import Ember from 'ember';
 export default Ember.ObjectController.extend({
 	taskSort: [ 'description' ],
 	sortedTasks: Ember.computed.sort('tasks', 'taskSort'),
+
+	newTaskDescription: '',
+	isPast: function() {
+		var date = this.get('date');
+		var now  = moment();
+		return (date.isBefore(now, 'day') && !date.isSame(now, 'day'));
+	}.property('date'),
+
+	isCurrent: function() {
+		return this.get('date').isSame(moment(), 'day');
+	}.property('date'),
+
+	isFuture: function() {
+		return this.get('date').isAfter(moment(), 'day');
+	}.property('date'),
+
 	actions: {
 		addTask: function() {
 			var taskDescription = this.get('newTaskDescription').trim();
@@ -28,7 +44,5 @@ export default Ember.ObjectController.extend({
 				task.save();
 			});
 		}
-	},
-
-	newTaskDescription: ''
+	}
 });
