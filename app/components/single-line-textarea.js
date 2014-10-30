d10bfae5278a149fd0239b4c9e21595400279136
Phil Gengler <phil@pgengler.net>
2014-10-29 20:55:35 -0400

    Use === instead of ==

--- app/components/single-line-textarea.js
+++ app/components/single-line-textarea.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.TextArea.extend({
 	keyDown: function(event) {
 		// Prevent newlines when using bare Enter key
-		if (event.which == 13 && !event.shiftKey) {
+		if (event.which === 13 && !event.shiftKey) {
 			event.preventDefault();
 		}
 	},
