5c31ecf9c2df94be258c325bcad34dc670ac4a8a
Phil Gengler <phil@pgengler.net>
2014-11-13 15:57:08 -0500

    Refresh 'days' model every 5 seconds in non-test environments

--- app/routes/days.js
+++ app/routes/days.js
@@ -7,5 +7,10 @@ export default Ember.Route.extend({
 
 	model: function(params) {
 		return this.store.find('day', params);
+	},
+	afterModel: function() {
+		if (!Ember.testing) {
+			Ember.run.later(this, this.refresh, 5000);
+		}
 	}
 });
