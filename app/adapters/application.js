34f7b8961651432d0a2261a7da062347073b2e75
Phil Gengler <phil@pgengler.net>
2014-11-12 21:27:22 -0500

    Use better name for timezone offset header

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -3,6 +3,6 @@ import DS from 'ember-data';
 export default DS.ActiveModelAdapter.extend({
 	namespace: 'api/v1',
 	headers: {
-		CLIENT_TIMEZONE: (-1 * (new Date()).getTimezoneOffset())
+		'X-Client-Timezone-Offset': (-1 * (new Date()).getTimezoneOffset())
 	}
 });
