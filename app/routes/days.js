29948ef71d572d2cc2b56cdb2bce434357650245
Phil Gengler <phil@pgengler.net>
2014-11-13 15:59:23 -0500

    Revert auto-refreshing of 'days'

--- app/routes/days.js
+++ app/routes/days.js
@@ -7,10 +7,5 @@ export default Ember.Route.extend({
 
 	model: function(params) {
 		return this.store.find('day', params);
-	},
-	afterModel: function() {
-		if (!Ember.testing) {
-			Ember.run.later(this, this.refresh, 5000);
-		}
 	}
 });
