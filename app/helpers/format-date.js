171f4ac202c29397482d72d4438f571cb56f0946
Phil Gengler <phil@pgengler.net>
2015-01-02 12:48:25 -0500

    Bleh.

--- app/helpers/format-date.js
+++ app/helpers/format-date.js
@@ -1,6 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.HTMLBars.makeBoundHelper(function(params) {
-	var date = params[0];
-	return moment(date).format("MMM D, YYYY");
+	return moment(params[0]).format("MMM D, YYYY");
 });
