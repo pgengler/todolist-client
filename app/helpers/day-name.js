632a0086bbdea32da0584105025b906056f3b827
Phil Gengler <phil@pgengler.net>
2015-01-02 11:52:51 -0500

    Upgrade ember to 1.10.0-beta.3

--- app/helpers/day-name.js
+++ app/helpers/day-name.js
@@ -1,5 +1,6 @@
 import Ember from 'ember';
 
-export default Ember.Handlebars.makeBoundHelper(function(date) {
+export default Ember.HTMLBars.makeBoundHelper(function(params) {
+	var date = params[0];
 	return moment(date).format('dddd');
 });
