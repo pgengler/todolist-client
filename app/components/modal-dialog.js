80efdad5259853aa803b93843b0ea20e415efba0
Phil Gengler <phil@pgengler.net>
2015-01-28 22:26:55 -0500

    Close modal dialog when 'Escape' is pressed

--- app/components/modal-dialog.js
+++ app/components/modal-dialog.js
@@ -2,6 +2,17 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 	className: 'modal-dialog',
+	didInsertElement: function() {
+		var component = this;
+		this.$().on('keydown', function(event) {
+			if (event.which === 27) {
+				component.triggerAction({
+					action: 'close',
+					target: component
+				});
+			}
+		});
+	},
 	actions: {
 		close: function() {
 			return this.sendAction('close');
