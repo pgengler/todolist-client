f96e4a2430dba88fc2aa3a9e0a01805dcc4efcfa
Phil Gengler <phil@pgengler.net>
2015-02-02 08:07:57 -0500

    Extract 'task-list' component for basic rendering of a column and layer 'day-tasks' on top of it, with day-specific CSS class support

--- app/components/day-tasks.js
+++ app/components/day-tasks.js
@@ -1,49 +1,8 @@
-import Ember from 'ember';
-import { filterBy, sortBy } from '../utils/computed';
+import TaskList from './task-list';
 
-function plaintext(str)
-{
-	return str.replace(/[^A-Za-z0-9]/g, '');
-}
-
-export default Ember.Component.extend({
-	classNames: [ 'task-list', 'spec-day' ],
-	classNameBindings: [ 'hasUnfinishedTasks' ],
-	day: null,
-	newTaskDescription: '',
-
-	finishedTasks: filterBy('day.tasks', 'done', true),
-	sortedFinishedTasks: sortBy('finishedTasks', 'description', plaintext),
-	unfinishedTasks: filterBy('day.tasks', 'done', false),
-	sortedUnfinishedTasks: sortBy('unfinishedTasks', 'description', plaintext),
-
-	hasUnfinishedTasks: Ember.computed.notEmpty('unfinishedTasks'),
-
-	actions: {
-		addTask: function() {
-			var taskDescription = this.get('newTaskDescription').trim();
-			if (!taskDescription) {
-				return;
-			}
-			var self = this;
-			var day = this.get('day');
-			this.store.createRecord('task', {
-				description: taskDescription,
-				day: day
-			}).save().then(function(task) {
-				day.get('tasks').addObject(task);
-				self.set('newTaskDescription', '');
-			});
-		},
-
-		moveTaskToDay: function(id) {
-			var day = this.get('day');
-			this.store.find('task', id).then(function(task) {
-				task.set('day', day);
-				task.save();
-			});
-		}
-	},
+export default TaskList.extend({
+	classNameBindings: [ 'isPast:past', 'isCurrent:current', 'isFuture:future' ],
+	layoutName: 'components/task-list',
 
 	isPast: function() {
 		var date = this.get('day.date');
@@ -62,5 +21,5 @@ export default Ember.Component.extend({
 		var now = moment();
 		now.subtract(now.zone(), 'minutes').utc();
 		return this.get('day.date').isAfter(now, 'day');
-	}.property('day.date'),
+	}.property('day.date')
 });
---
+++ app/components/task-list.js
@@ -0,0 +1,47 @@
+import Ember from 'ember';
+import { filterBy, sortBy } from '../utils/computed';
+
+function plaintext(str)
+{
+	return str.replace(/[^A-Za-z0-9]/g, '');
+}
+
+export default Ember.Component.extend({
+	classNames: [ 'task-list', 'spec-day' ],
+	classNameBindings: [ 'hasUnfinishedTasks' ],
+	day: null,
+	newTaskDescription: '',
+
+	finishedTasks: filterBy('day.tasks', 'done', true),
+	sortedFinishedTasks: sortBy('finishedTasks', 'description', plaintext),
+	unfinishedTasks: filterBy('day.tasks', 'done', false),
+	sortedUnfinishedTasks: sortBy('unfinishedTasks', 'description', plaintext),
+
+	hasUnfinishedTasks: Ember.computed.notEmpty('unfinishedTasks'),
+
+	actions: {
+		addTask: function() {
+			var taskDescription = this.get('newTaskDescription').trim();
+			if (!taskDescription) {
+				return;
+			}
+			var self = this;
+			var day = this.get('day');
+			this.store.createRecord('task', {
+				description: taskDescription,
+				day: day
+			}).save().then(function(task) {
+				day.get('tasks').addObject(task);
+				self.set('newTaskDescription', '');
+			});
+		},
+
+		moveTaskToDay: function(id) {
+			var day = this.get('day');
+			this.store.find('task', id).then(function(task) {
+				task.set('day', day);
+				task.save();
+			});
+		}
+	}
+});
