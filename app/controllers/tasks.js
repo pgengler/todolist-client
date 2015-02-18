90a5e3655c28962fd00eb39921b458b71d77abd0
Phil Gengler <phil@pgengler.net>
2015-02-17 19:26:16 -0500

    Remove unused 'tasks' controller

--- app/controllers/tasks.js
+++
@@ -1,7 +0,0 @@
-import Ember from 'ember';
-
-export default Ember.Controller.extend({
-	sortProperties: [ 'description' ],
-	tasks: Ember.computed.alias('model'),
-	sortedTasks: Ember.computed.sort('tasks', 'sortProperties')
-});
