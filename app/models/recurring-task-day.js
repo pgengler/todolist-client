cb2239d22ab02c1162f7a1a183f09772309ebdef
Phil Gengler <phil@pgengler.net>
2015-11-10 16:37:13 -0500

    Explicitly specify async relationships

--- app/models/recurring-task-day.js
+++ app/models/recurring-task-day.js
@@ -3,5 +3,5 @@ import DS from 'ember-data';
 export default DS.Model.extend({
 	day: DS.attr('string'),
 
-	tasks: DS.hasMany('recurringTask')
+	tasks: DS.hasMany('recurringTask', { async: false })
 });
