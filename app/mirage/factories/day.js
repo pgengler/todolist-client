4dfc3aaa576fe9ddab317cf836e67f6a7e31ee53
Phil Gengler <phil@pgengler.net>
2015-11-19 16:01:16 -0500

    Format auto-created dates in tests as YYYY-MM-DD as they would be in a real app (avoids issues with moment.js)

--- app/mirage/factories/day.js
+++ app/mirage/factories/day.js
@@ -1,6 +1,6 @@
 import Mirage from 'ember-cli-mirage';
 
 export default Mirage.Factory.extend({
-	date: (i) => moment().subtract(i, 'days'),
+	date: (i) => moment().subtract(i, 'days').format('YYYY-MM-DD'),
 	task_ids: [ ]
 });
