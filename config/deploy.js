4eb0640c2c8c3afa591146059058be5fc57958e6
Phil Gengler <phil@pgengler.net>
2017-02-26 22:59:04 -0500

    Use ember-cli-deploy for deployment

---
+++ config/deploy.js
@@ -0,0 +1,31 @@
+/* jshint node: true */
+
+module.exports = function(deployTarget) {
+  var ENV = {
+    build: {}
+    // include other plugin configuration that applies to all deploy targets here
+  };
+
+  ENV['with-rsync'] = {
+    username: 'todolist',
+    root: '/srv/apps/todolist/client'
+  };
+
+  if (deployTarget === 'development') {
+    ENV.build.environment = 'development';
+  }
+
+  if (deployTarget === 'staging') {
+    ENV.build.environment = 'production';
+  }
+
+  if (deployTarget === 'production') {
+    ENV.build.environment = 'production';
+    ENV['with-rsync'].host = 'hyperion.pgengler.net';
+  }
+
+  // Note: if you need to build some configuration asynchronously, you can return
+  // a promise that resolves with the ENV object instead of returning the
+  // ENV object synchronously.
+  return ENV;
+};
