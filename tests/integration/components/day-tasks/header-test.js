82f65951527ee69dea776a663196d14a54f8b265
Phil Gengler <phil@pgengler.net>
2018-01-06 08:31:08 -0500

    Run ember-qunit-codemod to convert integration tests to new syntax

--- tests/integration/components/day-tasks/header-test.js
+++ tests/integration/components/day-tasks/header-test.js
@@ -1,20 +1,22 @@
-import { moduleForComponent, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 import EmberObject from '@ember/object';
 
-moduleForComponent('day-tasks/header', 'Integration | Component | day tasks/header', {
-  integration: true
-});
+module('Integration | Component | day tasks/header', function(hooks) {
+  setupRenderingTest(hooks);
 
-test('it renders the day and formatted date', function(assert) {
-  let list = EmberObject.create({
-    name: '2017-12-31'
+  test('it renders the day and formatted date', async function(assert) {
+    let list = EmberObject.create({
+      name: '2017-12-31'
+    });
+    this.set('list', list);
+    await render(hbs`
+      {{day-tasks/header list=list}}
+    `);
+
+    assert.equal(this.$('h1').text().trim(), 'Sunday');
+    assert.equal(this.$('h2').text().trim(), 'Dec 31, 2017');
   });
-  this.set('list', list);
-  this.render(hbs`
-    {{day-tasks/header list=list}}
-  `);
-
-  assert.equal(this.$('h1').text().trim(), 'Sunday');
-  assert.equal(this.$('h2').text().trim(), 'Dec 31, 2017');
 });
--- tests/integration/components/task-list/header-test.js
+++ tests/integration/components/task-list/header-test.js
@@ -1,20 +1,22 @@
-import { moduleForComponent, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 import EmberObject from '@ember/object';
 
-moduleForComponent('task-list/header', 'Integration | Component | task list/header', {
-  integration: true
-});
+module('Integration | Component | task list/header', function(hooks) {
+  setupRenderingTest(hooks);
 
-test('it renders the list name', function(assert) {
-  let list = EmberObject.create({
-    name: 'Foo Bar Baz'
+  test('it renders the list name', async function(assert) {
+    let list = EmberObject.create({
+      name: 'Foo Bar Baz'
+    });
+    this.set('list', list);
+    await render(hbs`
+      {{task-list/header list=list}}
+    `);
+
+    assert.equal(this.$('.task-list-header').length, 1, 'has header class');
+    assert.equal(this.$('h1').text().trim(), 'Foo Bar Baz', 'renders list name');
   });
-  this.set('list', list);
-  this.render(hbs`
-    {{task-list/header list=list}}
-  `);
-
-  assert.equal(this.$('.task-list-header').length, 1, 'has header class');
-  assert.equal(this.$('h1').text().trim(), 'Foo Bar Baz', 'renders list name');
 });
