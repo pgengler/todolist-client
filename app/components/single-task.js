a45f8fffeb69f0b4286f24fb0320445e2dd2b255
Phil Gengler <phil@pgengler.net>
2018-03-03 07:58:13 -0500

    Make tasks editable in mobile Safari by sending fake dblclick events on double-taps

--- app/components/single-task.js
+++ app/components/single-task.js
@@ -26,6 +26,53 @@ export default Component.extend({
     }
   },
 
+  touchEnd(event) {
+    let now = Date.now();
+    let touch = event.changedTouches[0];
+
+    let lastTouchEndEventInfo = this.get('lastTouchEndEventInfo');
+    this.set('lastTouchEndEventInfo', {
+      clientX: touch.clientX,
+      clientY: touch.clientY,
+      when: now
+    });
+    if (!lastTouchEndEventInfo) {
+      return;
+    }
+
+    if (now - lastTouchEndEventInfo.when < 500) {
+      let xDistance = Math.abs(lastTouchEndEventInfo.clientX - touch.clientX);
+      let yDistance = Math.abs(lastTouchEndEventInfo.clientY - touch.clientY);
+
+      if (xDistance < 40 && yDistance < 40) {
+        let doubleClickEvent = document.createEvent('MouseEvents');
+        doubleClickEvent.initMouseEvent(
+          'dblclick',
+          true, // click bubbles
+          true, // click cancelable
+          event.view, // copy view
+          2,  // click count
+          // copy coordinates
+          touch.screenX,
+          touch.screenY,
+          touch.clientX,
+          touch.clientY,
+          // copy key modifiers
+          event.ctrlKey,
+          event.altKey,
+          event.shiftKey,
+          event.metaKey,
+          event.button, // copy button 0: left, 1: middle, 2: right
+          event.relatedTarget // copy relatedTarget
+        );
+
+        event.target.dispatchEvent(doubleClickEvent);
+        event.stopPropagation();
+        event.preventDefault();
+      }
+    }
+  },
+
   dragStart(event) {
     event.dataTransfer.setData('text/data', this.get('task.id'));
   },
