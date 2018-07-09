30b4fd35fa9918a4f4fc5faa264e6684adfe1aa6
Phil Gengler <phil@pgengler.net>
2018-07-09 08:41:03 -0400

    Remove deprecated ember-simple-auth authorizer stuff

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -3,5 +3,8 @@ import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
 export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
   namespace: 'api/v2',
-  authorizer: 'authorizer:application'
+  authorize(xhr) {
+    let { access_token } = this.get('session.data.authenticated');
+    xhr.setRequestHeader('Authorization', `Bearer ${access_token}`);
+  }
 });
