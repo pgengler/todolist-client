b85ab3b95c2a9a5b0c8635b02981e2ac4e4fe308
Phil Gengler <phil@pgengler.net>
2019-08-07 21:53:18 -0400

    Upgrade ember-cli-mirage to 1.1.0

--- tests/helpers/setup-acceptance-test.js
+++ tests/helpers/setup-acceptance-test.js
@@ -1,5 +1,5 @@
 import { setupApplicationTest } from 'ember-qunit';
-import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
+import { setupMirage } from 'ember-cli-mirage/test-support';
 
 export default function setupAcceptanceTest(hooks) {
   setupApplicationTest(hooks);
