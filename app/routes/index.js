63998b7d042fef4e740cbc342edc9db47414c81f
Phil Gengler <phil@pgengler.net>
2019-09-09 08:23:36 -0400

    Convert index route to be a native class

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,14 +1,14 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
 
-export default Route.extend({
-  session: service(),
+export default class IndexRoute extends Route {
+  @service session;
 
   redirect() {
-    if (this.get('session.isAuthenticated')) {
+    if (this.session.isAuthenticated) {
       this.transitionTo('days');
     } else {
       this.transitionTo('login');
     }
   }
-});
+}
