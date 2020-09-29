299ccb9af0d4236886bf9da56c2886e178995e24
Phil Gengler <phil@pgengler.net>
2020-09-29 13:46:48 -0400

    Update ApplicationAdapter to work with fetch & ember-simple-auth changes

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,10 +1,27 @@
+import { computed } from '@ember/object';
+import { inject as service } from '@ember/service';
 import JSONAPIAdapter from '@ember-data/adapter/json-api';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
-export default JSONAPIAdapter.extend(DataAdapterMixin, {
-  namespace: 'api/v2',
-  authorize(xhr) {
-    let { access_token } = this.session.data.authenticated;
-    xhr.setRequestHeader('Authorization', `Bearer ${access_token}`);
+export default class ApplicationAdapter extends JSONAPIAdapter {
+  @service session;
+
+  namespace = 'api/v2';
+
+  @computed('session.data.authenticated.access_token')
+  get headers() {
+    const headers = {};
+    if (this.session.isAuthenticated) {
+      headers['Authorization'] = `Bearer ${this.session.data.authenticated.access_token}`;
+    }
+
+    return headers;
   }
-});
+
+  handleResponse(status/*, headers, payload, requestData */) {
+    if (status === 401 && this.session.isAuthenticated) {
+      this.session.invalidate();
+    }
+    return super.handleResponse(...arguments);
+  }
+}
