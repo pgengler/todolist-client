3697dcfae511a72d622a800bfd70b33bb0d6b6e8
Phil Gengler <phil@pgengler.net>
2021-04-11 18:54:59 -0400

    Handle implicit injection deprecations

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,6 +3,7 @@ import { action } from '@ember/object';
 import { inject as service } from '@ember/service';
 
 export default class ApplicationController extends Controller {
+  @service flashMessages;
   @service router;
   @service selectedDate;
   @service session;
--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -1,5 +1,22 @@
 /* global self */
 self.deprecationWorkflow = self.deprecationWorkflow || {};
 self.deprecationWorkflow.config = {
-  workflow: [{ handler: 'silence', matchId: 'ember-metal.get-with-default' }],
+  workflow: [
+    {
+      handler: 'silence',
+      matchMessage: /A value was injected implicitly on the 'store' property of an instance of <ember-todo@session:main::(.+?)>/,
+    },
+    {
+      handler: 'silence',
+      matchMessage: /A value was injected implicitly on the 'session' property of an instance of <ember-todo@service:session::(.+?)>/,
+    },
+    {
+      handler: 'silence',
+      matchMessage: /A value was injected implicitly on the 'destinationElementId' property of an instance of <ember-todo@service:modal-dialog::(.+?)>, overwriting the original value which was null./,
+    },
+    { handler: 'silence', matchId: 'ember-metal.get-with-default' },
+    { handler: 'silence', matchId: 'manager-capabilities.modifiers-3-13' },
+    { handler: 'silence', matchId: 'this-property-fallback' },
+    { handler: 'silence', matchId: 'routing.transition-methods' },
+  ],
 };
