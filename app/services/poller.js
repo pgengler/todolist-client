cf6fe8098e9fe0ce91cbb834bc8a6b18a072672e
Phil Gengler <phil@pgengler.net>
2022-04-03 08:36:10 -0400

    Update 'service' imports

--- app/services/poller.js
+++ app/services/poller.js
@@ -1,4 +1,4 @@
-import Service, { inject as service } from '@ember/service';
+import Service, { service } from '@ember/service';
 import { all, didCancel, restartableTask, timeout } from 'ember-concurrency';
 import { tracked } from '@glimmer/tracking';
 import { isTesting, macroCondition } from '@embroider/macros';
