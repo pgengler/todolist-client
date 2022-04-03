844d406e7690e79d1941b8c4ba5c1ca3bceb1d80
Phil Gengler <phil@pgengler.net>
2022-04-03 08:14:10 -0400

    Address remaining deprecations

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -2,46 +2,9 @@
 self.deprecationWorkflow = self.deprecationWorkflow || {};
 self.deprecationWorkflow.config = {
   workflow: [
-    {
-      handler: 'silence',
-      matchMessage:
-        /A value was injected implicitly on the 'store' property of an instance of <ember-todo@session:main::(.+?)>/,
-    },
-    {
-      handler: 'silence',
-      matchMessage:
-        /A value was injected implicitly on the 'session' property of an instance of <ember-todo@service:session::(.+?)>/,
-    },
-    {
-      handler: 'silence',
-      matchMessage:
-        /A value was injected implicitly on the 'destinationElementId' property of an instance of <ember-todo@service:modal-dialog::(.+?)>, overwriting the original value which was null./,
-    },
-    {
-      handler: 'silence',
-      matchMessage:
-        /The `html` property was used in a template for the `ember-cli-showdown\/templates\/components\/markdown-to-html.hbs` component without using `this`./,
-    },
-    {
-      handler: 'silence',
-      matchMessage:
-        /The `(.+?)` property path was used in a template for the `ember-modal-dialog\/templates\/components\/modal-dialog.hbs` component without using `this`./,
-    },
-    {
-      handler: 'silence',
-      matchMessage:
-        /The `(.+?)` property (path )?was used in (a|the) template for the `ember-modal-dialog\/templates\/components\/basic-dialog.hbs` component without using `this`./,
-    },
-    {
-      handler: 'silence',
-      matchMessage:
-        /The `(.+?)` property path was used in a template for the `ember-wormhole\/templates\/components\/ember-wormhole.hbs` component without using `this`./,
-    },
-    {
-      handler: 'silence',
-      matchMessage:
-        /The `(.+?)` property path was used in a template for the `ember-power-calendar\/templates\/components\/power-calendar\/nav.hbs` component without using `this`/,
-    },
-    { handler: 'silence', matchId: 'manager-capabilities.modifiers-3-13' },
+    { handler: 'silence', matchId: 'ember-modifier.use-destroyables' },
+    { handler: 'silence', matchId: 'ember-modifier.use-modify' },
+    { handler: 'silence', matchId: 'ember-modifier.no-args-property' },
+    { handler: 'silence', matchId: 'ember-modifier.no-element-property' },
   ],
 };
--- mirage/config.js
+++ mirage/config.js
@@ -1,4 +1,17 @@
-export default function () {
+import { discoverEmberDataModels } from 'ember-cli-mirage';
+import { createServer } from 'miragejs';
+
+export default function (config) {
+  let finalConfig = {
+    ...config,
+    models: { ...discoverEmberDataModels(), ...config.models },
+    routes,
+  };
+
+  return createServer(finalConfig);
+}
+
+function routes() {
   this.passthrough('/write-coverage');
 
   this.logging = true;
