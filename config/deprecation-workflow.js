b2fec45152e9bd12e0a52c7b5d11ffb1e7e2453e
Phil Gengler <phil@pgengler.net>
2022-04-30 08:46:36 -0400

    Remove no-longer-applicable entries from deprecation-workflow config

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -1,10 +1,5 @@
 /* global self */
 self.deprecationWorkflow = self.deprecationWorkflow || {};
 self.deprecationWorkflow.config = {
-  workflow: [
-    { handler: 'silence', matchId: 'ember-modifier.use-destroyables' },
-    { handler: 'silence', matchId: 'ember-modifier.use-modify' },
-    { handler: 'silence', matchId: 'ember-modifier.no-args-property' },
-    { handler: 'silence', matchId: 'ember-modifier.no-element-property' },
-  ],
+  workflow: [],
 };
