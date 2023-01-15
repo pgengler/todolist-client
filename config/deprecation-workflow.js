a30b42fd452633cbb33366987bd9f3a864caf292
Phil Gengler <phil@pgengler.net>
2023-01-15 10:55:10 -0500

    Upgrade ember-source to 4.10.0

--- config/deprecation-workflow.js
+++ config/deprecation-workflow.js
@@ -5,6 +5,5 @@ self.deprecationWorkflow.config = {
   workflow: [
     // ember-data:deprecate-early-static is happening because of ember-cli-mirage
     { handler: 'silence', matchId: 'ember-data:deprecate-early-static' },
-    { handler: 'silence', matchId: 'ember-data:deprecate-promise-proxies', matchMessage: /__ec_cancel/ },
   ],
 };
