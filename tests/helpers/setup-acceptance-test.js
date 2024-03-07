685a25118eba4898f8345ab23996b2efe7b1faf8
Phil Gengler <phil@pgengler.net>
2024-03-06 21:28:22 -0500

    Upgrade ember-cli-mirage to 3.0.3 and miragejs to 0.1.48

--- tests/helpers/setup-acceptance-test.js
+++ tests/helpers/setup-acceptance-test.js
@@ -1,5 +1,5 @@
 import { setupApplicationTest } from 'ember-qunit';
-import { setupMirage } from '@pgengler/ember-cli-mirage/test-support';
+import { setupMirage } from 'ember-cli-mirage/test-support';
 
 export default function setupAcceptanceTest(hooks) {
   setupApplicationTest(hooks);
