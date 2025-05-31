dacba704396dbc8c5987c3b7a92b97c8c7beb37f
Phil Gengler <phil@pgengler.net>
2025-05-31 15:12:51 -0400

    Run ember-cli-update to upgrade to Ember 6.4

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -3,12 +3,14 @@ import config from 'ember-todo/config/environment';
 import * as QUnit from 'qunit';
 import { setApplication } from '@ember/test-helpers';
 import { setup } from 'qunit-dom';
-import { start } from 'ember-qunit';
+import { loadTests } from 'ember-qunit/test-loader';
+import { start, setupEmberOnerrorValidation } from 'ember-qunit';
 
 import './helpers/flash-message';
 
 setApplication(Application.create(config.APP));
 
 setup(QUnit.assert);
-
+setupEmberOnerrorValidation();
+loadTests();
 start();
