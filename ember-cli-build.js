d0260a9308352d9a46e5f58b3ed9da9a73d69395
Phil Gengler <phil@pgengler.net>
2025-12-28 00:22:46 -0500

    Fix ember-data config

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -5,18 +5,17 @@ const { compatBuild } = require('@embroider/compat');
 
 module.exports = async function (defaults) {
   const { buildOnce } = await import('@embroider/vite');
+  const { setConfig } = await import('@warp-drive/build-config');
+
   let app = new EmberApp(defaults, {
-    emberData: {
-      deprecations: {
-        // New projects can safely leave this deprecation disabled.
-        // If upgrading, to opt-into the deprecated behavior, set this to true and then follow:
-        // https://deprecations.emberjs.com/id/ember-data-deprecate-store-extends-ember-object
-        // before upgrading to Ember Data 6.0
-        DEPRECATE_STORE_EXTENDS_EMBER_OBJECT: false,
-      },
-    },
     'ember-simple-auth': {
       useSessionSetupMethod: true,
+    },
+  });
+
+  setConfig(app, __dirname, {
+    deprecations: {
+      DEPRECATE_TRACKING_PACKAGE: false,
     },
   });
 
